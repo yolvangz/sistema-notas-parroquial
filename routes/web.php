1aaea71a58f686da3c68b297bce3918ac9d6a004
Yolvan G. Zambrano <yolvanzg@gmail.com>
2025-05-18 20:29:32 -0400

    Añadidas las rutas para profesores, añadida ruta y vista para listar letras cedula como prueba

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,26 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Profesores;
+use Illuminate\Support\Facades\DB;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/letras-cedula', function () {
+    $letrasCedula = DB::table('letrasCedula')->orderBy('IDLetraCedula')->select('IDLetraCedula as id', 'letra', 'nombre')->get();
+    return view('letrasCedula', ['letrasCedula' => $letrasCedula]);
+});
+
+Route::controller(Profesores::class)->group(function () {
+    Route::prefix('profesores')->group(function () {
+        Route::get('/', 'index')->name('profesores.index');
+        Route::get('/nuevo', 'create')->name('profesores.create');
+        Route::post('/', 'store')->name('profesores.store');
+        Route::get('/{id}', 'show')->name('profesores.show');
+        Route::get('/{id}/editar', 'edit')->name('profesores.edit');
+        Route::put('/{id}', 'update')->name('profesores.update');
+        Route::delete('/{id}', 'destroy')->name('profesores.destroy');
+    });
 });
